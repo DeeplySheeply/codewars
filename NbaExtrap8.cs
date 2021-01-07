d1a24568eabb4987f29f19f7e54a34dcb995a18d
DeeplySheeply <62748916+DeeplySheeply@users.noreply.github.com>
2021-01-07 16:52:47 +0000

    Update and rename NbaExtrap.cs to NbaExtrap8.cs

--- NbaExtrap8.cs
+++ NbaExtrap8.cs
@@ -1,3 +1,5 @@
+// Problem can be found here: https://www.codewars.com/kata/587c2d08bb65b5e8040004fd/train/csharp
+
 class NbaExtrap
     {
         public static double NbaExtrap(double ppg, double mpg)
