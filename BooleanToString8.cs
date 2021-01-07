8f3cd91d1ddb090365b299c311c211372b96ad98
DeeplySheeply <62748916+DeeplySheeply@users.noreply.github.com>
2021-01-07 18:03:40 +0000

    Create BooleanToString8.cs

---
+++ BooleanToString8.cs
@@ -0,0 +1,12 @@
+// This problem can be found at https://www.codewars.com/kata/551b4501ac0447318f0009cd/train/csharp
+// This exercise demonstrates knowledge of ternary operators
+
+using System;
+
+public class kata
+{
+  public static string boolean_to_string(bool b)
+  {
+  return b ? "True" : "False";
+  }
+}
