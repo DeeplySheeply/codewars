909e6e436d0a646ffc30b7cdc1be7dd4524754f4
DeeplySheeply <62748916+DeeplySheeply@users.noreply.github.com>
2022-11-11 11:43:41 +0000

    Create shorterreverselonger.cs

---
+++ shorterreverselonger.cs
@@ -0,0 +1,35 @@
+ // Link to the challenge https://www.codewars.com/kata/54557d61126a00423b000a45/train/csharp
+ 
+ // Global variables
+    string a = "";
+    string b = "";
+    string finalResult = "";
+
+    private void ShorterReverseLongerMethod()
+    {
+        // Local variables 
+        int longest = a.Length;
+        int shortest = b.Length;
+        string temp = "";
+
+        // Check for nulls
+        if (string.IsNullOrEmpty(a))
+        {
+            a = "";
+        }
+
+        if (string.IsNullOrEmpty(b))
+        {
+            b = "";
+        }        
+
+        // If statement to solve the challenge
+        if (shortest > longest)
+        {
+            temp = temp + a;
+            a = b;
+            b = temp;
+        }
+
+         finalResult = string.Concat(b, string.Concat(a.Reverse()), b);
+    }
